5485d4a238817fed4949f6d4d2312ad1fd7ec8c4
Jea Perez <hi@jeaperez.com>
2024-06-27 10:35:12 +0800

    Update FilterAndFind.js

--- FilterAndFind.js
+++ FilterAndFind.js
@@ -1,25 +1,38 @@
-function updateDeposited() {
+function FilterAndFind() {
   var sourceSpreadsheetId = "1yOw4RBovjJWPGQI7Q5rv1efi5EV6imoh4yIDAYvJmmA";
-  var targetSpreadsheetId = "1lVff_u8mUlNLxSEelrjpw0OOceDrIxH7F4mnMQUhu5g";
+  var targetSpreadsheetId = "1SXs6PMrFO1Epr3HVuEpzdpw57ufuzFw9KNL_aWOsqok";
   
   var sourceSheetName = "UBP_7088 LUZON";
-  var targetSheetNames = ["NORTH LUZON - ALAND", "SOUTH LUZON - ALAND", "SOUTH LUZON - LIMA", "LUZON - ALAND"];
+  var targetSheetNames = ["NORTH LUZON - ALAND v2", "SOUTH LUZON - ALAND", "SOUTH LUZON - LIMA", "LUZON - ALAND", "LUZON - LIMA/CIPDI"];
+  var logSheetName = "logSheet"; 
 
   var sourceSpreadsheet = SpreadsheetApp.openById(sourceSpreadsheetId);
   var targetSpreadsheet = SpreadsheetApp.openById(targetSpreadsheetId);
   
   var sourceSheet = sourceSpreadsheet.getSheetByName(sourceSheetName);
+  var logSheet = targetSpreadsheet.getSheetByName(logSheetName);
+
+  logSheet.clear();
+  
+  var lastProcessedRowRange = logSheet.getRange("A1");
+  var lastProcessedRow = lastProcessedRowRange.getValue();
+  if (!lastProcessedRow) {
+    lastProcessedRow = 0;
+  }
 
   var sourceRange = sourceSheet.getDataRange();
   var sourceValues = sourceRange.getValues();
+  var richTextValues = sourceRange.getRichTextValues();
 
+  var newLogEntries = [];
   var copiedValuesToFind = [];
 
-  for (var i = 0; i < sourceValues.length; i++) {
-    if (sourceValues[i][8] > 0) {
-      var copiedValue = sourceValues[i][31]; 
-      if (copiedValue !== "" && !copiedValuesToFind.includes(copiedValue)) {
+  for (var i = lastProcessedRow; i < richTextValues.length; i++) {
+    if (isRedText(richTextValues[i][0], 1)) {
+      var copiedValue = sourceValues[i][31];
+      if (copiedValue !== "" && copiedValue !== "0000000000" && !copiedValuesToFind.includes(copiedValue)) {
         copiedValuesToFind.push(copiedValue);
+        newLogEntries.push([copiedValue]);
       }
     }
   }
@@ -31,28 +44,46 @@ function updateDeposited() {
       return;
     }
 
-    var targetColumnRange = targetSheet.getRange("AZ:AZ");
+    var targetColumnRange = targetSheet.getRange("AY:AY");
     var targetColumnValues = targetColumnRange.getValues();
     var targetStatuses = targetSheet.getRange("O:O").getValues();
-    var processedMarkers = targetSheet.getRange("BB:BB").getValues();
+    var processedMarkers = targetSheet.getRange("AZ:AZ").getValues();
 
     var updates = [];
 
     for (var rowIndex = 0; rowIndex < targetColumnValues.length; rowIndex++) {
       var cellValue = targetColumnValues[rowIndex][0];
-      if (copiedValuesToFind.includes(cellValue) && targetStatuses[rowIndex][0] !== "Deposited-RCD") {
+      var processedMarker = processedMarkers[rowIndex][0];
+      if (copiedValuesToFind.includes(cellValue) && processedMarker !== "Processed") {
         updates.push(rowIndex + 1);
       }
     }
 
     if (updates.length > 0) {
+      var bgRanges = targetSheet.getRangeList(updates.map(row => "AY" + row));
       var statusRanges = targetSheet.getRangeList(updates.map(row => "O" + row));
-      var markerRanges = targetSheet.getRangeList(updates.map(row => "BB" + row));
+      var markerRanges = targetSheet.getRangeList(updates.map(row => "AZ" + row));
 
-      statusRanges.setValue("Deposited-RCD");
-      markerRanges.setValue("Processed-Deposited");
+      bgRanges.setBackground('#FFFF00');
+      statusRanges.setValue("Bounced");
+      markerRanges.setValue("Processed");
     }
   });
 
-  Logger.log("Processed rows count: " + copiedValuesToFind.length);
+  if (newLogEntries.length > 0) {
+    var logSheetLastRow = logSheet.getLastRow();
+    logSheet.getRange(logSheetLastRow + 1, 1, newLogEntries.length, 1).setValues(newLogEntries);
+
+    lastProcessedRowRange.setValue(richTextValues.length);
+  }
+
+  Logger.log("Processed rows count: " + newLogEntries.length);
 }
+
+function isRedText(richTextValue, columnIndex) {
+  if (columnIndex === 1) {
+    return richTextValue.getTextStyle().getForegroundColor() === '#ff0000';
+  }
+  return false;
+}
+
